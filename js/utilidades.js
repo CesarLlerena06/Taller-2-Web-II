4fc76426a525f8374d9574bc9eb117388d374cf2
CesarLlerena06 <cesar.llerena@cecar.edu.co>
2024-05-06 09:09:37 -0500

    agregue

---
+++ js/utilidades.js
@@ -0,0 +1,12 @@
+function crearNodo(textoH1){
+
+    var h1 = document.createElement("h1")
+            var textoH1 = document.createTextNode("Ejemplo Javascipt")
+            h1.appendChild(textoH1)
+
+            document.body.appendChild(h1)
+}
+
+function createTextNode (textoH1 ,){
+
+}
